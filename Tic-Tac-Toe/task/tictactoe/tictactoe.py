4c3d4a0a5145dc9dd3a45c396231e5048a7babf2
Sergey Goppikov <grey2k@gmail.com>
2020-12-07 01:40:11 +0300

    3/5 Start

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,1 +1,59 @@
 # write your code here
+
+X = 'X'
+Z = 'O'
+_ = '_'
+
+
+def print_field(field: list) -> None:
+    print("---------")
+    for row in field:
+        print("| " + " ".join(row) + " |")
+    print("---------")
+
+
+def input_to_field(command: str) -> list:
+    current = 0
+    rows = 0
+    field = [
+        [_, _, _],
+        [_, _, _],
+        [_, _, _],
+    ]
+    for n in list(command):
+        if n == X:
+            symbol = X
+        elif n == Z:
+            symbol = Z
+        else:
+            symbol = _
+
+        field[rows][current] = symbol
+        current += 1
+        if current == 3:
+            current = 0
+            rows += 1
+
+    return field
+
+
+field0 = [
+    [X, Z, X],
+    [Z, _, Z],
+    [X, X, _],
+]
+
+field1 = [
+    [X, Z, X],
+    [Z, X, Z],
+    [X, X, Z],
+]
+
+# print_field(field0)
+# print('---')
+# print_field(field1)
+
+print("Enter cells:")
+cells = input()
+
+print_field(input_to_field(cells))
